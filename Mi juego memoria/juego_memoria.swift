893a6b9996277f5091bfc6c99d0d299a525a63e3
Xavi <nucaduck@gmail.com>
2016-06-23 12:43:55 +0200

    Subida

---
+++ Mi juego memoria/juego_memoria.swift
@@ -0,0 +1,28 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+var i = 0...100
+
+
+
+/*for numero in i {
+print(numero)
+}*/
+
+
+
+for numero in i {
+    if (numero % 5) == 0 {
+        print(numero, "Bingo!!!")
+    }
+    if (numero % 2) == 0 {
+        print(numero, "Par!!!")
+    }else {
+        print(numero, "Impar!!!")
+    }
+    if (numero>=30 && numero<=40){
+        print(numero, "Viva Swift!!!")
+    }
+}
